4a225a4c3a03dcfd2286ac91adcd1b0455f8ecd9
horih <seikanbunsiun.17@gmail.com>
2023-11-29 23:29:28 +0900

    edit .gitignore

--- out/_next/static/chunks/pages/Components-16545e671c6c1b2e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[46],{1020:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Components",function(){return u(2692)}])}},function(n){n.O(0,[766,514,529,692,774,888,179],function(){return n(n.s=1020)}),_N_E=n.O()}]);
